09fd164d05d73f8f7d28db81b1bd73ef3a5c210f
rikuddo91 <gullkhan0@live.com>
2022-12-06 23:46:53 +0100

    Editing routes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,7 +10,7 @@ use DateTime;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
+    #[Route('/', name: 'app_main')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
