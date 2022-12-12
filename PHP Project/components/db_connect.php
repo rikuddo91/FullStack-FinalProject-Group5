a9998566f0c3626b9728022c4570c81fe6a11c0f
AbdullahAlHraishawi <abdullah.alhraishawi@gmail.com>
2022-12-12 13:36:11 +0100

    File name modified

--- PHP Project/components/db_connect.php
+++ PHP Project/components/db_connect.php
@@ -3,7 +3,7 @@
 $hostname = "127.0.0.1";
 $username = "root";
 $password = "";
-$dbname = "fswd_g5_meal_planner_project";
+$dbname = "fswd_g5_meal_planner";
 
 // create connection
 $connect = mysqli_connect($hostname, $username, $password, $dbname);
