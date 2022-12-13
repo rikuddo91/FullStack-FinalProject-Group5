3ca2cb2aa239946bb5cc6eb88b570758e77a13c9
Quarsatos <manuelpapula69@gmail.com>
2022-12-13 11:21:52 +0100

    changed icon colors

--- PHP Project/footer.php
+++ PHP Project/footer.php
@@ -1,11 +1,11 @@
 <footer class="mt-5">
   <div class="text-center mt-2 bg-dark text-white d-flex flex-column pt-3">
     <div class="d-flex justify-content-center gap-3 fs-2">
-      <a href=""><i class="bi bi-facebook"></i></a>
-      <a href=""><i class="bi bi-instagram"></i></a>
-      <a href=""><i class="bi bi-github"></i></a>
-      <a href=""><i class="bi bi-twitter"></i></a>
-      <a href=""><i class="bi bi-youtube"></i></a>          
+      <a id="footer_icons" href=""><i class="bi bi-facebook"></i></a>
+      <a id="footer_icons" href=""><i class="bi bi-instagram"></i></a>
+      <a id="footer_icons" href=""><i class="bi bi-github"></i></a>
+      <a id="footer_icons" href=""><i class="bi bi-twitter"></i></a>
+      <a id="footer_icons" href=""><i class="bi bi-youtube"></i></a>          
     </div>
       <p>&copy;2022 Copyright:</br>
       Junaid - Abdullah - Magdalena - Ibrahim - Manuel</p>
