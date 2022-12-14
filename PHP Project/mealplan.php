d05413389c21f4892d4822e04d429a7c3e6085a2
Quarsatos <manuelpapula69@gmail.com>
2022-12-14 13:26:33 +0100

    small changes

--- PHP Project/mealplan.php
+++ PHP Project/mealplan.php
@@ -18,15 +18,15 @@ $tbody = '';
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
         $tbody .= "<tr>
-                <td><img class='img-thumbnail' src='" . $row['picture'] . "'</td>
+                <td id='meal_row'><img class='img-thumbnail' src='" . $row['picture'] . "'</td>
                 <td id='meal_row'>" . $row['name'] . "</td>
                 <td id='meal_row'>" . $row['calories'] . "</td>
                 <td id='meal_row'>" . $row['prep_time'] . "</td>
                 <td id='meal_row'>" . $row['diet'] . "</td>
                 <td id='meal_row'>" . $row['type'] . "</td>
                 <td id='meal_row'>" . $row['date'] . "</td>
-                <td><a href='details.php?id=" . $row['fk_recipe_id'] . "'><button class='btn btn-primary' type='button'>Info</button></a>
-                <a href='actions/a_mealplan_delete.php?id=" . $row['fk_recipe_id'] . "'><button class='btn btn-danger'>Delete from plan</button></a></td>
+                <td id='meal_row'><a href='details.php?id=" . $row['fk_recipe_id'] . "'><button class='btn btn-primary' type='button'>Info</button></a>
+                <a href='actions/a_mealplan_delete.php?id=" . $row['fk_recipe_id'] . "'><button class='btn btn-danger'>Delete</button></a></td>
                 </tr>";
     }
 } else {
@@ -52,8 +52,8 @@ mysqli_close($connect);
 
 <body>
 <?php require_once 'navbar.php' ?>
-    <div>
-        <p>Your Meal plan</p>
+    <div class="container-fluid d-flex flex-column text-center">
+        <h2 class="m-4">Your Meal plan</h2>
         <table class='table table-striped'>
             <thead class='table-secondary text-nowrap'>
                 <tr>
