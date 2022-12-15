f97049addc796b58f421cc3c87f0dfdfcb80e33f
Quarsatos <manuelpapula69@gmail.com>
2022-12-15 11:01:29 +0100

    added footer links

--- PHP Project/components/footer.php
+++ PHP Project/components/footer.php
@@ -1,11 +1,11 @@
 <footer class="mt-5">
   <div class="text-center mt-2 bg-dark text-white d-flex flex-column pt-3">
     <div class="d-flex justify-content-center gap-3 fs-2">
-      <a id="footer_icons" href=""><i class="bi bi-facebook"></i></a>
-      <a id="footer_icons" href=""><i class="bi bi-instagram"></i></a>
-      <a id="footer_icons" href=""><i class="bi bi-github"></i></a>
-      <a id="footer_icons" href=""><i class="bi bi-twitter"></i></a>
-      <a id="footer_icons" href=""><i class="bi bi-youtube"></i></a>          
+      <a id="footer_icons" href="https://www.facebook.com/" target="_blank"><i class="bi bi-facebook"></i></a>
+      <a id="footer_icons" href="https://www.instagram.com/" target="_blank"><i class="bi bi-instagram"></i></a>
+      <a id="footer_icons" href="https://github.com/" target="_blank"><i class="bi bi-github" ></i></a>
+      <a id="footer_icons" href="https://twitter.com/" target="_blank"><i class="bi bi-twitter" ></i></a>
+      <a id="footer_icons" href="https://www.youtube.com/" target="_blank"><i class="bi bi-youtube" ></i></a>          
     </div>
       <p>&copy;2022 Copyright:</br>
       Junaid - Abdullah - Magdalena - Ibrahim - Manuel</p>
